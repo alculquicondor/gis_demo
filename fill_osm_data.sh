d326ca5fb41ff34e5b96e3c229780075f8220139
Aldo Culquicondor <alculquicondor@gmail.com>
2016-09-02 13:29:37 -0500

    adding shebang

--- fill_osm_data.sh
+++ fill_osm_data.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 wget http://download.geofabrik.de/south-america/peru-latest.osm.pbf
 PROJECT_NAME=$(basename `pwd`)
 POSTGIS_IP=$(docker inspect --format "{{ .NetworkSettings.Networks.${PROJECT_NAME}_default.IPAddress }}" "${PROJECT_NAME}_postgis_1") 
