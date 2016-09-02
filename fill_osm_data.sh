d280f7bf308e236c3c9d87de9f1912e1e314c55d
Aldo Culquicondor <alculquicondor@gmail.com>
2016-09-02 14:41:48 -0500

    speed up osm2pgsql

--- fill_osm_data.sh
+++ fill_osm_data.sh
@@ -5,4 +5,4 @@ PROJECT_NAME=$(basename `pwd` | tr -cd '[[:alnum:]]')
 POSTGIS_IP=$(docker inspect --format "{{ .NetworkSettings.Networks.${PROJECT_NAME}_default.IPAddress }}" "${PROJECT_NAME}_postgis_1") 
 echo 'NOTE: get password from docker-compose.yml'
 echo $POSTGIS_IP
-osm2pgsql -s -U topicos -d topicos -H $POSTGIS_IP -W peru-latest.osm.pbf
+osm2pgsql -U topicos -d topicos -H $POSTGIS_IP -W peru-latest.osm.pbf
