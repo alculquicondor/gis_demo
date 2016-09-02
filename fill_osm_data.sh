2fae4ad7bcc91d895c7418cca70c21eaaa698827
Aldo Culquicondor <alculquicondor@gmail.com>
2016-09-02 14:38:22 -0500

    bugfix on obtaining project name

--- fill_osm_data.sh
+++ fill_osm_data.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 wget http://download.geofabrik.de/south-america/peru-latest.osm.pbf
-PROJECT_NAME=$(basename `pwd`)
+PROJECT_NAME=$(basename `pwd` | tr -cd '[[:alnum:]]')
 POSTGIS_IP=$(docker inspect --format "{{ .NetworkSettings.Networks.${PROJECT_NAME}_default.IPAddress }}" "${PROJECT_NAME}_postgis_1") 
 echo 'NOTE: get password from docker-compose.yml'
+echo $POSTGIS_IP
 osm2pgsql -s -U topicos -d topicos -H $POSTGIS_IP -W peru-latest.osm.pbf
