d3588dcd8c620da85a7a206a52183531eb6725f0
Aldo Culquicondor <alculquicondor@gmail.com>
2016-09-07 14:13:30 -0500

    names in english

--- setup/areas_and_places.sql
+++ setup/areas_and_places.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS gisdemo_area;
 DROP TABLE IF EXISTS gisdemo_place;
 
 CREATE TABLE gisdemo_area (
-  id SERIAL, nombre TEXT NOT NULL);
+  id SERIAL, name TEXT NOT NULL);
 SELECT AddGeometryColumn('gisdemo_area', 'geometry', 4326, 'POLYGON', 2);
 
 CREATE TABLE gisdemo_place (
-  id SERIAL, nombre TEXT NOT NULL);
+  id SERIAL, name TEXT NOT NULL);
 SELECT AddGeometryColumn('gisdemo_place', 'geometry', 4326, 'POINT', 2);
 
 COMMIT;
